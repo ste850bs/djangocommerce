9ddaf624d17956d8337071980e3ca8888a78f664
pierangelo1982 <pierangelo1982@gmail.com>
2016-07-27 18:24:48 +0200

    products backend

--- product/admin.py
+++ product/admin.py
@@ -3,14 +3,27 @@ from django.contrib import admin
 from product.models import *
 from image_cropping import ImageCroppingMixin
 
-def get_category(self):
-    return self.categoria.titolo
 
-'''
+def get_category(self):
+    return self.category.title
+
+
 class MyModelAdmin(ImageCroppingMixin, admin.ModelAdmin):
     pass
 
 
-class SliderAdmin(ImageCroppingMixin, admin.ModelAdmin):
-    list_display = ("image_img", "titolo", "active", "azienda")
-'''
+class ProductAdmin(ImageCroppingMixin, admin.ModelAdmin):
+    list_display = ("image_img", "code", "name", "price", "discount", "price_offer", "prompt_delivery", "delivery", "promo", "active")
+
+
+
+
+admin.site.register(Category, MyModelAdmin)
+admin.site.register(Color, MyModelAdmin)
+admin.site.register(Material, MyModelAdmin)
+admin.site.register(Accessory, MyModelAdmin)
+admin.site.register(Product, MyModelAdmin)
+admin.site.register(Composition, MyModelAdmin)
+
+
+
--- product/models.py
+++ product/models.py
@@ -10,3 +10,253 @@ from taggit.managers import TaggableManager
 from tinymce.models import HTMLField
 from django.utils import timezone
 from django import forms
+
+
+class Category(models.Model):
+    title = models.CharField('titolo', max_length=100)
+    subtitle = models.CharField('sottotitolo', max_length=250, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.titolo
+
+    class Meta:
+        verbose_name_plural = "Categorie"
+
+
+
+
+class Color(models.Model):
+    name = models.CharField('nome colore', max_length=100)
+    code = models.CharField('codice colore', max_length=250, null=True, blank=True)
+    css_color = models.CharField('css colore', max_length=250, null=True, blank=True)
+    image = image = models.ImageField('immagine colore', blank=True, null=True, upload_to='color')
+
+    def __unicode__(self):
+        return self.titolo
+
+    class Meta:
+        verbose_name_plural = "Colori"
+
+
+
+
+class Material(models.Model):
+    name = models.CharField('nome colore', max_length=100)
+    image = image = models.ImageField('immagine colore', blank=True, null=True, upload_to='color')
+    description = models.TextField('descrizione', null=True, blank=True)
+
+    def __unicode__(self):
+        return self.titolo
+
+    class Meta:
+        verbose_name_plural = "Colori"
+
+
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100)
+    link = models.CharField(max_length=250, null=True, blank=True)
+    image = models.ImageField(blank=True, null=True, upload_to='marche')
+    thumb = ImageRatioField('image', '312x135', verbose_name="Miniatura")
+
+    def image_img(self):
+        if self.image:
+            return u'<img src="%s" style="width:200px"/>' % self.image.url
+        else:
+            return '(Sin imagen)'
+    image_img.short_description = 'Thumb'
+    image_img.allow_tags = True
+
+    def __unicode__(self):
+        return self.titolo
+
+    class Meta:
+        verbose_name_plural = "Produttore"
+
+
+
+
+class Accessory(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Titolo:")
+    code = models.CharField('Codice', max_length=250, null=True, blank=True)
+    category = models.ManyToManyField(Category, null=True, blank=True, verbose_name="Seleziona Categorie")
+    manufacturer = models.ForeignKey(Manufacturer, null=True, blank=True, verbose_name="Marca")
+    designer = models.CharField(max_length=250, null=True, blank=True)
+    ##PRICE
+    price = models.DecimalField('Prezzo', max_digits=10, decimal_places=2, blank=True, null=True)
+    discount = models.IntegerField(blank=True, null=True, verbose_name="sconto percentuale")
+    price_offer = models.DecimalField('Prezzo Scontato', max_digits=10, decimal_places=2, blank=True, null=True)
+    ##MULTIMEDIA
+    image = models.ImageField(blank=True, null=True, upload_to='accessory', verbose_name="Immagine")
+    slider = ImageRatioField('image', '1170x600', verbose_name="Slider")
+    thumb = ImageRatioField('image', '600x449', verbose_name="Miniatura")
+    thumbdue = ImageRatioField('image', '745x558', verbose_name="Miniatura pagina dettaglio")
+    croplibero = ImageRatioField('image', '595x335', free_crop=True, verbose_name="Ritaglio Libero")
+    album = FilerFolderField(null=True, blank=True)
+    ## Composition
+    color = models.ManyToManyField(Color, null=True, blank=True, verbose_name="Seleziona Colori")
+    material = models.ManyToManyField(Material, null=True, blank=True, verbose_name="Seleziona Materiali")
+    ## Data
+    quantity = models.IntegerField(blank=True, null=True, verbose_name="sconto percentuale")
+    size = models.CharField('Misure', max_length=250, null=True, blank=True)
+    width = models.IntegerField(blank=True, null=True, verbose_name="larghezza")
+    lenght = models.IntegerField(blank=True, null=True, verbose_name="lunghezza")
+    depth = models.IntegerField(blank=True, null=True, verbose_name="Profondita")
+    height = models.IntegerField(blank=True, null=True, verbose_name="altezza")
+    volume = models.DecimalField('Volume', max_digits=10, decimal_places=2, blank=True, null=True)
+    models.IntegerField(blank=True, null=True, verbose_name="sconto percentuale")
+    descrizione = models.TextField(null=True, blank=True, verbose_name="MATERIALE/FINITURA/COLORE")
+    ## Delivery
+    prompt_delivery = models.BooleanField('Pronta Consegna', default=False)
+    delivery = models.BooleanField('Consegna 40gg', default=False)
+
+    tags = TaggableManager(verbose_name="Parole chiave", blank=True)
+    pub_date = models.DateTimeField('date published')
+    active = models.BooleanField('attiva', default=False)
+    slide = models.BooleanField('Mostra in Slide', default=False)
+    promo = models.BooleanField('Mostra in Promo', default=False)
+
+
+
+    def image_img(self):
+        if self.image:
+            return u'<img src="%s" style="width:300px"/>' % self.image.url
+        else:
+            return '(Sin imagen)'
+    image_img.short_description = 'Thumb'
+    image_img.allow_tags = True
+
+    def __unicode__(self):
+        return self.titolo
+
+    class Meta:
+        verbose_name_plural = "Accessori"
+        ordering = ['id']
+
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Titolo:")
+    code = models.CharField('Codice', max_length=250, null=True, blank=True)
+    category = models.ManyToManyField(Category, null=True, blank=True, verbose_name="Seleziona Categorie")
+    manufacturer = models.ForeignKey(Manufacturer, null=True, blank=True, verbose_name="Marca")
+    designer = models.CharField(max_length=250, null=True, blank=True)
+    ##PRICE
+    price = models.DecimalField('Prezzo', max_digits=10, decimal_places=2, blank=True, null=True)
+    discount = models.IntegerField(blank=True, null=True, verbose_name="sconto percentuale")
+    price_offer = models.DecimalField('Prezzo Scontato', max_digits=10, decimal_places=2, blank=True, null=True)
+    ##MULTIMEDIA
+    image = models.ImageField(blank=True, null=True, upload_to='product', verbose_name="Immagine")
+    slider = ImageRatioField('image', '1170x600', verbose_name="Slider")
+    thumb = ImageRatioField('image', '600x449', verbose_name="Miniatura")
+    thumbdue = ImageRatioField('image', '745x558', verbose_name="Miniatura pagina dettaglio")
+    croplibero = ImageRatioField('image', '595x335', free_crop=True, verbose_name="Ritaglio Libero")
+    album = FilerFolderField(null=True, blank=True)
+    ## Composition
+    color = models.ManyToManyField(Color, null=True, blank=True, verbose_name="Seleziona Colori")
+    material = models.ManyToManyField(Material, null=True, blank=True, verbose_name="Seleziona Materiali")
+    ## Data
+    quantity = models.IntegerField(blank=True, null=True, verbose_name="sconto percentuale")
+    size = models.CharField('Misure', max_length=250, null=True, blank=True)
+    width = models.IntegerField(blank=True, null=True, verbose_name="larghezza")
+    lenght = models.IntegerField(blank=True, null=True, verbose_name="lunghezza")
+    depth = models.IntegerField(blank=True, null=True, verbose_name="Profondita")
+    height = models.IntegerField(blank=True, null=True, verbose_name="altezza")
+    volume = models.DecimalField('Volume', max_digits=10, decimal_places=2, blank=True, null=True)
+    models.IntegerField(blank=True, null=True, verbose_name="sconto percentuale")
+    descrizione = models.TextField(null=True, blank=True, verbose_name="MATERIALE/FINITURA/COLORE")
+    ## Delivery
+    prompt_delivery = models.BooleanField('Pronta Consegna', default=False)
+    delivery = models.BooleanField('Consegna 40gg', default=False)
+    ## Accessory
+    material = models.ManyToManyField(Accessory, null=True, blank=True, verbose_name="Accessori")
+
+    tags = TaggableManager(verbose_name="Parole chiave", blank=True)
+    pub_date = models.DateTimeField('date published')
+    active = models.BooleanField('attiva', default=False)
+    slide = models.BooleanField('Mostra in Slide', default=False)
+    promo = models.BooleanField('Mostra in Promo', default=False)
+
+
+
+    def image_img(self):
+        if self.image:
+            return u'<img src="%s" style="width:300px"/>' % self.image.url
+        else:
+            return '(Sin imagen)'
+    image_img.short_description = 'Thumb'
+    image_img.allow_tags = True
+
+    def __unicode__(self):
+        return self.titolo
+
+    class Meta:
+        verbose_name_plural = "Prodotti"
+        ordering = ['id']
+
+
+
+
+class Composition(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Titolo:")
+    code = models.CharField('Codice', max_length=250, null=True, blank=True)
+    category = models.ManyToManyField(Category, null=True, blank=True, verbose_name="Seleziona Categorie")
+    manufacturer = models.ForeignKey(Manufacturer, null=True, blank=True, verbose_name="Marca")
+    designer = models.CharField(max_length=250, null=True, blank=True)
+
+    ##
+    ## Components
+    component = models.ManyToManyField(Product, null=True, blank=True, verbose_name="Componenti")
+    ##PRICE
+    price = models.DecimalField('Prezzo', max_digits=10, decimal_places=2, blank=True, null=True)
+    discount = models.IntegerField(blank=True, null=True, verbose_name="sconto percentuale")
+    price_offer = models.DecimalField('Prezzo Scontato', max_digits=10, decimal_places=2, blank=True, null=True)
+    ##MULTIMEDIA
+    image = models.ImageField(blank=True, null=True, upload_to='product', verbose_name="Immagine")
+    slider = ImageRatioField('image', '1170x600', verbose_name="Slider")
+    thumb = ImageRatioField('image', '600x449', verbose_name="Miniatura")
+    thumbdue = ImageRatioField('image', '745x558', verbose_name="Miniatura pagina dettaglio")
+    croplibero = ImageRatioField('image', '595x335', free_crop=True, verbose_name="Ritaglio Libero")
+    album = FilerFolderField(null=True, blank=True)
+    ## Composition
+    color = models.ManyToManyField(Color, null=True, blank=True, verbose_name="Seleziona Colori")
+    material = models.ManyToManyField(Material, null=True, blank=True, verbose_name="Seleziona Materiali")
+    ## Data
+    quantity = models.IntegerField(blank=True, null=True, verbose_name="sconto percentuale")
+    size = models.CharField('Misure', max_length=250, null=True, blank=True)
+    width = models.IntegerField(blank=True, null=True, verbose_name="larghezza")
+    lenght = models.IntegerField(blank=True, null=True, verbose_name="lunghezza")
+    depth = models.IntegerField(blank=True, null=True, verbose_name="Profondita")
+    height = models.IntegerField(blank=True, null=True, verbose_name="altezza")
+    volume = models.DecimalField('Volume', max_digits=10, decimal_places=2, blank=True, null=True)
+    models.IntegerField(blank=True, null=True, verbose_name="sconto percentuale")
+    descrizione = models.TextField(null=True, blank=True, verbose_name="MATERIALE/FINITURA/COLORE")
+    ## Delivery
+    prompt_delivery = models.BooleanField('Pronta Consegna', default=False)
+    delivery = models.BooleanField('Consegna 40gg', default=False)
+   
+    tags = TaggableManager(verbose_name="Parole chiave", blank=True)
+    pub_date = models.DateTimeField('date published')
+    active = models.BooleanField('attiva', default=False)
+    slide = models.BooleanField('Mostra in Slide', default=False)
+    promo = models.BooleanField('Mostra in Promo', default=False)
+
+    def image_img(self):
+        if self.image:
+            return u'<img src="%s" style="width:300px"/>' % self.image.url
+        else:
+            return '(Sin imagen)'
+    image_img.short_description = 'Thumb'
+    image_img.allow_tags = True
+
+    def __unicode__(self):
+        return self.titolo
+
+    class Meta:
+        verbose_name_plural = "Composizioni"
+        ordering = ['id']
+
+
+
